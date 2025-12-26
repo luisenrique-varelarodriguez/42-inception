34da0a440c2ad0e11feaa280ca8f16fdf8935230
Luis Enrique Varela Rodríguez <luisenrique.varelarodriguez@telefonica.com>
2025-12-26 13:01:07 +0100

    nginx: dockerfile & script refactor, conf fix

--- srcs/requirements/nginx/tools/script.sh
+++ srcs/requirements/nginx/tools/script.sh
@@ -1,36 +1,29 @@
 #!/bin/bash
+set -e
 
-# Variables
-DAYS_VALID=365
+if [ -z "$DOMAIN_NAME" ]; then
+    echo "Error: DOMAIN_NAME not set"
+    exit 1
+fi
+
+# SSL certificate variables
 CERT_DIR="/etc/ssl/nginx"
 CERT_NAME="sscert.crt"
 KEY_NAME="sskey.key"
-SUBJECT="/C=ES/ST=Madrid/L=Madrid/CN=$DOMAIN_NAME"
 
-# Creates directories if they don't exists
-mkdir -p $CERT_DIR
+mkdir -p "$CERT_DIR"
 
-# Check if the certificate and key already exist
-if [ -f "$CERT_DIR/$CERT_NAME" ] && [ -f "$CERT_DIR/$KEY_NAME" ]; then
-    echo "Certificate and private key already exist. Skipping generation."
-else
-    # Generates the certificate and the private key
-    openssl req -x509 -nodes -days $DAYS_VALID -newkey rsa:2048 -keyout $CERT_DIR/$KEY_NAME -out $CERT_DIR/$CERT_NAME -subj "$SUBJECT"
-
-    # Verify if the generation was successful
-    if [ $? -eq 0 ]; then
-        echo "Certificate and private key successfully generated."
-        echo "Certificate: $CERT_DIR/$CERT_NAME"
-        echo "Private key: $CERT_DIR/$KEY_NAME"
-    else
-        echo "Error generating the certificate and private key." >&2
-        exit 1
-    fi
+# Generate SSL certificate if it doesn't exist
+if [ ! -f "$CERT_DIR/$CERT_NAME" ] || [ ! -f "$CERT_DIR/$KEY_NAME" ]; then
+    openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
+        -keyout "$CERT_DIR/$KEY_NAME" \
+        -out "$CERT_DIR/$CERT_NAME" \
+        -subj "/C=ES/ST=Madrid/L=Madrid/CN=$DOMAIN_NAME"
 fi
 
-# Move the default.conf file to the correct location
+# Move NGINX configuration
 if [ -f "/default.conf" ]; then
-    mv /default.conf /etc/nginx/conf.d/default.conf
+    envsubst '${DOMAIN_NAME}' < /default.conf > /etc/nginx/conf.d/default.conf # Use envsubst to replace DOMAIN_NAME
 fi
 
 exec nginx -g "daemon off;"
